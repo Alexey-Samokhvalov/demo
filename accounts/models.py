5f1c963279b4de46c7bfd94df7e2244b89cdfa5b
Alexey-Samokhvalov <alex.samoxvalov1@gmail.com>
2025-03-29 04:06:15 +0300

    Первый коммит

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
- username = models.OneToOneField(User, on_delete=models.CASCADE)
+ user = models.OneToOneField(User, on_delete=models.CASCADE)
  password = models.CharField(max_length=100)
  full_name = models.CharField(max_length=150)
  email = models.EmailField()
